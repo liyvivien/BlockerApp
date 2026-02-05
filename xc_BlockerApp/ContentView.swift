b67e59504c3a1c9a4b67aaef20de59b7879887de
ooooonionlee <vivien199696@gmail.com>
2026-02-05 14:57:12 +0000

    Initial Commit

---
+++ xc_BlockerApp/ContentView.swift
@@ -0,0 +1,26 @@
+//
+//  ContentView.swift
+//  xc_BlockerApp
+//
+//  Created by 🌊 on 05/02/2026.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundColor(.accentColor)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ xc_BlockerApp/xc_BlockerAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  xc_BlockerAppApp.swift
+//  xc_BlockerApp
+//
+//  Created by 🌊 on 05/02/2026.
+//
+
+import SwiftUI
+
+@main
+struct xc_BlockerAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
